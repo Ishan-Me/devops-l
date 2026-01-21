2fdd5e750741df96b93a82b155f5993337f1e006
Ishan <ishanpatel0429@gmail.com>
2026-01-21 06:49:24 +0000

    Update hello.sh in dev branch

--- hello.sh
+++ hello.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # This is a comment (ignored by the system)
 
-echo "Enter your name:"
-read user_name
-read user_lname
-
-echo "hello $user_name $user_lname, Welcome to devops!"
+echo "Hello from dev branch"
 
